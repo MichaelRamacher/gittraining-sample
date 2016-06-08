ac61a50bcb0cbc6fa2b7c441879d011df94da2c3
Michael Ramacher <michael.ramacher@generali.com>
2016-06-08 14:00:55 +0200

    erste Änderung

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -13,6 +13,8 @@ import org.eclipse.example.calc.BinaryOperation;
 import org.eclipse.example.calc.Operation;
 import org.eclipse.example.calc.Operations;
 import org.eclipse.example.calc.UnaryOperation;
+import org.eclipse.example.calc.internal.operations.Clear;
+import org.eclipse.example.calc.internal.operations.Divide;
 import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
@@ -40,6 +42,8 @@ public class Calculator {
 		new Minus();
 		new Plus();
 		new Square();
+		new Divide();
+		new Clear();
 	}
 
 	private void calculate(String cmdName) {
